1f21e14f5ebb165bf8dd311bc7687f543228a351
Bharadwaj <chickkubharadwajsarma@gmail.com>
2025-06-15 19:36:40 +0530

    moving-server-into-api

---
+++ server/api/server.js
@@ -0,0 +1,33 @@
+import '../config/instrument.js'
+import express from 'express'
+import cors from 'cors'
+import * as Sentry from "@sentry/node"
+import 'dotenv/config'
+import connectDB from '../config/db.js'
+import { clerkWebhooks } from '../controllers/webhooks.js'
+import bodyParser from 'body-parser'
+import serverless from 'serverless-http'
+
+const app = express()
+
+// Connect to database
+await connectDB()
+
+app.use(cors())
+
+// Webhook route (required to be under /api in Vercel)
+app.post('/api/webhooks', bodyParser.raw({ type: 'application/json' }), clerkWebhooks)
+
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+  res.send('API is working')
+})
+
+app.get("/api/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!")
+})
+
+Sentry.setupExpressErrorHandler(app)
+
+export const handler = serverless(app)
