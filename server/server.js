b08324b1f6e0eb382829b5be4f4142b071fd313a
Bharadwaj <chickkubharadwajsarma@gmail.com>
2025-07-03 23:46:51 +0530

    hope

--- server/server.js
+++ server/server.js
@@ -1,44 +1,46 @@
-import express from 'express';
-import cors from 'cors';
-import * as Sentry from "@sentry/node";
-import 'dotenv/config';
-import connectDB from './config/db.js';
-import connectCloudinary from './config/cloudinary.js';
-import companyRoutes from './routes/companyRoutes.js';
-import jobRoutes from './routes/jobRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import { clerkMiddleware } from '@clerk/express';
-import { clerkWebhooks } from './controllers/webhooks.js';
+import './config/instrument.js'
+import express from 'express'
+import cors from 'cors'
+import * as Sentry from "@sentry/node"
+import 'dotenv/config'
+import connectDB from './config/db.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
+import companyRoutes from './routes/companyRoutes.js'
+import connectCloudinary from './config/cloudinary.js'
+import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
+// Initialize Express
+const app = express()
 
-// Import raw from express for Clerk
-import bodyParser from 'body-parser';
+//Connect to database
+await connectDB()
+await connectCloudinary()
 
-const app = express();
+//Middlewares
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
 
-await connectDB();
-await connectCloudinary();
-
-app.use(cors());
-
-// ✅ USE RAW ONLY FOR WEBHOOK FIRST
-app.post('/webhooks', bodyParser.raw({ type: '*/*' }), clerkWebhooks);
-
-// ✅ THEN use json body parser after webhook
-app.use(express.json());
-app.use(clerkMiddleware());
-
-// 🔁 Routes
+//Routes
 app.get('/', (req, res) => {
   res.send('API is working');
 });
-app.use('/api/company', companyRoutes);
-app.use('/api/jobs', jobRoutes);
-app.use('/api/users', userRoutes);
 
-// Vercel server listen
-const PORT = process.env.PORT || 5000;
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+app.post('/webhooks', clerkWebhooks)
+app.use('/api/company',companyRoutes)
+app.use('/api/jobs',jobRoutes)
+app.use('/api/users',userRoutes)
+
+//Port
+const PORT = process.env.PORT || 5000
+
 Sentry.setupExpressErrorHandler(app);
 
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
